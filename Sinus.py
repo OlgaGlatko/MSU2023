1a3dd2ab2d08c6b42b09381bfc22dbabab674106
OlgaGlatko <olga03.glatko@gmail.com>
2023-04-24 09:58:53 +0300

    Homework

--- Sinus.py
+++ Sinus.py
@@ -2,8 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 from math import *
 
-
-# специальная функция - отрисовка канваса по параметрам\n",
 def my_actionr():
     
     frm = int(e1.get())
@@ -22,29 +20,23 @@ def my_actiong():
     to   = int(e2.get())
     for i in range( frm, to):
         canvas.create_line(i,10*sin(i)+150, i+1, 10*sin(i)+150, fill='green', width=10)
-# а теперь за главное окно будет отвечать переменная master\n",
+
 master = Tk()
 master.title('Рисуем')
 
 canvas = Canvas(master, width=800, height=400, background='black')
 canvas.grid(column=0, row=1)
 
-# виджет Label - просто надпись\n",
 l1 = Label(master, text='t0')
 l1.grid(row=2, column=0, sticky='e')
 l2 = Label(master, text='t1')
 l2.grid(row=3, column=0, sticky='e')
 
-# виджет Entry - поле ввода текстовой информации. \n",
 e1 = Entry(master)
 e2 = Entry(master)
 e1.grid(row=2, column=1)
 e2.grid(row=3, column=1)
 
-
-# виджет Button() - кнопка с текстом, нажатие на которую вызывает \n",
-# выполнение команды-функции, переданной в параметрах. Сама функция\n",
-# описана 19 строками выше\n",
 b1 = Button(master, text="Draw red!", command = my_actionr)
 b1.grid(row=4, column = 1)
 
